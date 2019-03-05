3eafe29988f574f00eb09857414d4a7ca949c920
Eason Chen <e.chen@glowing.com>
2019-03-05 10:35:19 +0800

    Add sync script

---
+++ gitToLocal.sh
@@ -0,0 +1,2 @@
+cp ./init.vim ~/.config/nvim/init.vim
+cp ./settings.json ~/Library/Application\ Support/Code/User/settings.json
---
+++ localToGit.sh
@@ -0,0 +1,3 @@
+cp ~/.config/nvim/init.vim init.vim
+cp ~/Library/Application\ Support/Code/User/settings.json settings.json
+
